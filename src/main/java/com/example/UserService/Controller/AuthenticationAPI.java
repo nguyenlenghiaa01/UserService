dc1aeb9e6537bf6c34ca728cae7a06ff3951d1ad
CongNghia1 <nguyenlenghiawork@gmail.com>
2025-06-25 17:04:06 +0700

    fix(err cors) : remove cross

--- src/main/java/com/example/UserService/Controller/AuthenticationAPI.java
+++ src/main/java/com/example/UserService/Controller/AuthenticationAPI.java
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/users")
 @SecurityRequirement(name = "api")
-@CrossOrigin("*")
 public class AuthenticationAPI {
 
     ModelMapper modelMapper = new ModelMapper();
