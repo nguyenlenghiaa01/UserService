8c6bdc6fed5ec7293cdbf1b56c94401c7f2e0f89
CongNghia1 <nguyenlenghiawork@gmail.com>
2025-07-21 02:49:43 +0700

    fix(report-service) : add gate for report

---
+++ src/main/java/com/example/UserService/Config/OpenFeignClientInterceptor.java
@@ -0,0 +1,25 @@
+package com.example.UserService.Config;
+
+
+import feign.RequestInterceptor;
+import feign.RequestTemplate;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Component
+public class OpenFeignClientInterceptor implements RequestInterceptor {
+
+    @Override
+    public void apply(RequestTemplate requestTemplate) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        if (attributes != null) {
+            HttpServletRequest request = attributes.getRequest();
+            String authHeader = request.getHeader("Authorization");
+            if (authHeader != null) {
+                requestTemplate.header("Authorization", authHeader);
+            }
+        }
+    }
+}
